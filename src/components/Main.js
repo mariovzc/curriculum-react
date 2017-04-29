c270df4b31de13d6cf7c813c07c694cbf2f8e998
¨Mario <mario.vizcainos@gmail.com>
2017-04-28 22:54:24 -0500

    created header component and export it to the view

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
-
+import HeaderComponent from './HeaderComponent';
 let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
@@ -11,6 +11,7 @@ class AppComponent extends React.Component {
       <div className="index">
         <img src={yeomanImage} alt="Yeoman Generator" />
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+        <HeaderComponent/>
       </div>
     );
   }
