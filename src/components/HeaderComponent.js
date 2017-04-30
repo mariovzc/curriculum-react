5650f7aa9f62adb52cd222e420c17786ea98b25e
¨Mario <mario.vizcainos@gmail.com>
2017-04-29 23:11:57 -0500

    refactor of the header component list

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -3,7 +3,12 @@
 import React from 'react';
 
 require('styles//Header.css');
-
+const data = [
+  {source:'https://image.flaticon.com/icons/svg/214/214316.svg', alt:'email', class: 'social email', val: 'mario.vizcainos@gmail.com', id:1},
+  {source:'https://image.flaticon.com/icons/svg/149/149004.svg', alt:'phone', class: 'social phone', val: '(+57) 3164293230', id:2},
+  {source:'https://image.flaticon.com/icons/svg/174/174869.svg', alt:'skype', class: 'social skype', val: 'mario.vizcainos', id:3},
+  {source:'https://image.flaticon.com/icons/svg/174/174869.svg', alt:'github', class: 'social github', val: 'mariovzc', id:4}
+];
 class HeaderComponent extends React.Component {
   render() {
     return (
@@ -19,24 +24,14 @@ class HeaderComponent extends React.Component {
               <img src="https://gravatar.com/avatar/67f1ef5b57b0581f1bd097c49d2888d3?s=512&d=https://codepen.io/assets/avatars/user-avatar-512x512-6e240cf350d2f1cc07c2bed234c3a3bb5f1b237023c204c782622e80d6b212ba.png" className="img-fluid profile-picture" alt="Responsive image " />
             </div>
             <div className="col-sm-4 text-right social-container">
-                <div className="social email">
-                  <b>mario.vizcainos@gmail.com</b>
-                  <img src="https://image.flaticon.com/icons/svg/214/214316.svg" alt="email" className="social-image" />
+              {data.map((social) =>
+                <div key={social.id}  className={social.class}>
+                  <b>{social.val}</b>
+                  <img src={social.source} alt={social.alt} className="social-image" />
                 </div>
-                <div className="social phone">
-                  <b>(+57) 3164293230</b>
-                  <img src="https://image.flaticon.com/icons/svg/149/149004.svg" alt="" className="social-image" />
-                </div>
-                <div className="social skype">
-                  <b>mario.vizcainos</b>
-                  <img src="https://image.flaticon.com/icons/svg/174/174869.svg" alt="" className="social-image" />
-                </div>
-                <div className="social github">
-                  <b>mariovzc</b>
-                  <img src="https://image.flaticon.com/icons/svg/270/270798.svg" alt="" className="social-image" />
-                </div>
-              </div>
+              )}
             </div>
+          </div>
         </header>
       </div>
     );
